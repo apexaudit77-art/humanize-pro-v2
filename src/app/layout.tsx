c6ee186e39b06b9ae979a3fcc173e07c92fd708b
Saleh Hezam Alfaqeh <salehhezama@gmail.com>
2026-01-12 13:40:35 +0000

    قم بتحديث ملف src/app/layout.tsx لإضافة كود Google Tag Manager. ضع الكود

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from 'next';
 import { Inter, Space_Grotesk } from 'next/font/google';
 import './globals.css';
@@ -33,6 +34,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K3RHZ8XC');`,
+          }}
+        />
+      </head>
       <body
         className={cn(
           'min-h-screen bg-background font-body antialiased',
@@ -40,6 +52,14 @@ export default function RootLayout({
           spaceGrotesk.variable
         )}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K3RHZ8XC"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <FirebaseClientProvider>
           <ThemeProvider
             attribute="class"
