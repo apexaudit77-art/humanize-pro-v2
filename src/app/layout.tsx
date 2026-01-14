38a64ed77714ede12ec32fbf52945009a5fc1e12
Saleh Hezam Alfaqeh <salehhezama@gmail.com>
2026-01-14 17:20:19 +0000

    G-FR6P8X409N هذامعرف قوقل انالاتيكس الخاص بي ادمجه في الموقع

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,17 +48,19 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning data-scroll-behavior="smooth">
       <head>
-        {/* <!-- Google Tag Manager --> */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-K3RHZ8XC');`,
-          }}
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-FR6P8X409N"
         />
-        {/* <!-- End Google Tag Manager --> */}
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FR6P8X409N');
+          `}
+        </Script>
         
         {/* <!-- Google Publisher Center --> */}
         <script async type="application/javascript"
@@ -86,16 +88,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           spaceGrotesk.variable
         )}
       >
-        {/* <!-- Google Tag Manager (noscript) --> */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-K3RHZ8XC"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
-        {/* <!-- End Google Tag Manager (noscript) --> */}
         <FirebaseClientProvider config={firebaseConfig}>
           <ThemeProvider
             attribute="class"
