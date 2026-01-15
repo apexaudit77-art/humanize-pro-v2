dfc367875abe2d254ccc04c9a86798ad8e13e023
Saleh Hezam Alfaqeh <salehhezama@gmail.com>
2026-01-15 18:27:16 +0000

    "يظهر خطأ عند تسجيل الدخول بجوجل. يرجى التأكد من أن apiKey و authDomain

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,12 +33,12 @@ export default function RootLayout({
 }>) {
 
   const firebaseConfig = {
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    authDomain: "humanize-ai.ooguy.com",
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   };
 
   return (
