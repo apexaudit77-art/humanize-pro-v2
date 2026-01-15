ad48dba7ff906bb7381d5c345d9bc50b7266a824
Saleh Hezam Alfaqeh <salehhezama@gmail.com>
2026-01-15 08:16:29 +0000

    "أريد إجراء إصلاحات حاسمة للموقع لضمان الفهرسة وربط التحليلات. يرجى تنفي

--- src/app/rss.xml/route.ts
+++
@@ -1,2 +0,0 @@
-// This file is intentionally left blank as it has been replaced by a static file in /public/rss.xml
-// And will be removed in the next step.
--- src/scripts/generate-feeds.mjs
+++
@@ -1,2 +0,0 @@
-// This script is no longer needed as feeds are now handled statically or by other means.
-// It is being removed to simplify the build process.
