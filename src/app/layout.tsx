9a8d73f3a52380e7df404e0c76b85d2aa52d959b
Saleh Hezam Alfaqeh <salehhezama@gmail.com>
2026-01-15 11:54:21 +0000

    إضافة الأكواد التالية في src/app/layout.tsx:
    كود Header (في أعلى <head>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,17 +45,13 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning data-scroll-behavior="smooth">
       <head>
-        {/* Google Tag Manager */}
-        <Script id="gtm-head" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-K3RHZ8XC');
-          `}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-K3RHZ8XC');`}
         </Script>
-        {/* End Google Tag Manager */}
       </head>
       <body
         className={cn(
@@ -64,16 +60,7 @@ export default function RootLayout({
           spaceGrotesk.variable
         )}
       >
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-            <iframe
-                src="https://www.googletagmanager.com/ns.html?id=GTM-K3RHZ8XC"
-                height="0"
-                width="0"
-                style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K3RHZ8XC" height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
 
         <FirebaseClientProvider config={firebaseConfig}>
           <ThemeProvider
