667693faef3945376f01d9a984af1602d28b13aa
Saleh Hezam Alfaqeh <salehhezama@gmail.com>
2026-01-30 17:56:00 +0000

    أنا أواجه خطأ 'Internal Server Error' في واجهة المعاينة، وأريد منك إصلاح

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,7 +1,6 @@
 'use client';
 
 // This file is now a barrel that re-exports from the new central firebase lib and other modules.
-export * from '@/lib/firebase'; 
 export * from './provider';
 export * from './client-provider';
 export * from './firestore/use-collection';
@@ -9,4 +8,3 @@ export * from './firestore/use-doc';
 export * from './non-blocking-updates';
 export * from './errors';
 export * from './error-emitter';
-
