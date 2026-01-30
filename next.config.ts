1d33009d59adade051c59be504af8881915ccb38
Saleh Hezam Alfaqeh <salehhezama@gmail.com>
2026-01-30 15:11:53 +0000

    في واجهة المعاينه يظهر هذا Internal Server Error بدلا من الموقع

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
   serverExternalPackages: ['@genkit-ai/core', 'genkit'],
   webpack: (config, { isServer }) => {
     // This is to fix the "Module not found: Can't resolve 'dgram'" error and others.
